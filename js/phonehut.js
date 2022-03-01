627296521458d8f897fc22dd4e8e2cf3277efe7d
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-03-02 01:11:12 +0600

    phone found updated

--- js/phonehut.js
+++ js/phonehut.js
@@ -3,15 +3,44 @@ const main = document.getElementById('main');
 /* search button linked  */
 
 const searchButton = ()=>{
-    const input = document.getElementById('input-value')
-    const inputValue = input.value;
+    const searchBox = document.getElementById('input-value')
     const contentbox = document.getElementById('content-box')
-    if(inputValue !==''){
+    const searchText = searchBox.value;
+    /* if(searchText !==''){
         contentbox.innerText='Your Product found'
-        input.value=''
+        searchBox.value=''
     } 
-    else if(inputValue==''){
+    else if(searchText==''){
         contentbox.innerText='Your Product is not found'
-        input.value=''
+        searchBox.value=''
+    } */
+    
+        fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+        .then(res => res.json())
+        .then(data => displayPhones(data.data))
+        searchBox.value=''
+        contentbox.innerHTML=''
+
+}
+
+
+const displayPhones = (phones) =>{
+    
+    for(const phone of phones){
+        const div = document.createElement('div')
+        div.classList='col-lg-4'
+        
+        div.innerHTML=`
+                <div class="card" style="width: 18rem;">
+                    <img src="${phone.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h3 class="card-title">Name:${phone.phone_name}</h3>
+                    <p class="card-text">Brand: ${phone.brand}</p>
+                    <button href="#" class="btn btn-primary">See Details</button>
+                    </div>
+            </div>
+        `
+
+        main.appendChild(div)
     }
 }
